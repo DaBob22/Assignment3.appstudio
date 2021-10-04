4c1eb9dac95fcbb28dbee2b4a53ecdb0b225bbe9
DaBob22 <90647821+DaBob22@users.noreply.github.com>
2021-10-03 23:03:21 -0500

    Save 2

---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,10 @@
+/*
+let firstPassword = prompt("Enter a password: ")
+let secondPassword = prompt("Enter a second password: ")
+
+function compare(pass1, pass2) {
+   let isequal = pass1.localeCompare(pass2)
+   return (isequal == 0) ? "The passwords are the same": "The passwords are not the same"
+}
+console.log(compare(firstPassword, secondPassword))
+*/
